f8d3032dee97a535151b5513106037acee33d81f
ringtail003 <ringtail003@ringtail003-macstudio.local>
2023-08-15 10:52:19 +0900

    chore: node(ESM)のビルドまで

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+import { foo } from "./foo.js";
+import { fooM } from "./foo.mjs";
+foo();
+fooM();
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { foo } from "./foo.js";
+import { fooM } from "./foo.mjs";
+
+foo();
+fooM();
